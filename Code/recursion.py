f0a4d0294e53cdd4a12cea4ad5c42d0214545ca4
type9 <gabespersonal@gmail.com>
2020-01-28 17:06:48 -0800

    Added factorial_iterative

--- Code/recursion.py
+++ Code/recursion.py
@@ -8,8 +8,8 @@ def factorial(n):
         raise ValueError('factorial is undefined for n = {}'.format(n))
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_iterative(n)
+    # return factorial_recursive(n)
 
 
 def factorial_iterative(n):
@@ -17,6 +17,12 @@ def factorial_iterative(n):
     pass
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
+    total = 1
+    while n > 1:
+        total *= n
+        n -= 1
+    
+    return total
 
 
 def factorial_recursive(n):
