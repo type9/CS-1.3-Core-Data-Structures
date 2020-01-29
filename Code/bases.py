9b598d86917478be4b927419a8fbdbca4b58fb37
type9 <gabespersonal@gmail.com>
2020-01-28 16:17:22 -0800

    Added methods ot bases.py

--- Code/bases.py
+++ Code/bases.py
@@ -9,6 +9,25 @@ import string
 # string.ascii_letters is ascii_lowercase + ascii_uppercase
 # string.printable is digits + ascii_letters + punctuation + whitespace
 
+def enc36(num):
+    '''encodes the single base 10 digit as a single character up to 36'''
+    # Breakpoint
+    DIGIT = 9
+    #Checks
+    if num <= DIGIT: # already is base 10
+        return num
+    if num > DIGIT: # needs to be in a letter
+        letter_index = (num - DIGIT) - 1
+        return string.ascii_lowercase[letter_index]
+
+def dec36(digit):
+    '''decodes digit up to 36 as base 10'''
+    # Breakpoint
+    DIGIT = 9
+    try:
+        return int(digit)
+    except:
+        return string.ascii_lowercase.index(digit) + DIGIT # + 1 is for array index offset
 
 def decode(digits, base):
     """Decode given digits in given base to number in base 10.
@@ -23,7 +42,14 @@ def decode(digits, base):
     # ...
     # TODO: Decode digits from any base (2 up to 36)
     # ...
-
+    final = 0
+    pwr = 0
+    for digit in digits[::-1]:
+        final += dec36(digit) * (base ** pwr)
+        pwr += 1
+    
+    return final
+        
 
 def encode(number, base):
     """Encode given number in base 10 to digits in given base.
@@ -40,6 +66,13 @@ def encode(number, base):
     # ...
     # TODO: Encode number in any base (2 up to 36)
     # ...
+
+    final = ""
+    while number > 0:
+        number, rem = divmod(number, base)
+        final += str(enc36(rem)) # converts to its proper symbol if above base 10
+    
+    return final[::-1] # reverse string with slice notation
 
 
 def convert(digits, base1, base2):
@@ -59,12 +92,19 @@ def convert(digits, base1, base2):
     # ...
     # TODO: Convert digits from any base to any base (2 up to 36)
     # ...
+    normalized = decode(digits, base1)
+    print(normalized)
+    final = encode(normalized, base2)
+    return final
+
 
 
 def main():
     """Read command-line arguments and convert given digits between bases."""
     import sys
     args = sys.argv[1:]  # Ignore script file name
+    print(encode(42, 16))
+    print(decode(encode(42, 16), 16))
     if len(args) == 3:
         digits = args[0]
         base1 = int(args[1])
