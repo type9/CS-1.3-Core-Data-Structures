8d5d2c97dcf1c69559ae94ecf7462a044ce7cc09
type9 <gabespersonal@gmail.com>
2020-01-28 17:10:58 -0800

    Added annotations of time complexities

--- Code/search.py
+++ Code/search.py
@@ -1,7 +1,8 @@
 #!python
 
 def linear_search(array, item):
-    """return the first index of item in array or None if item is not found"""
+    """return the first index of item in array or None if item is not found.
+    Time complexity of n"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
     return linear_search_iterative(array, item)
@@ -9,6 +10,7 @@ def linear_search(array, item):
 
 
 def linear_search_iterative(array, item):
+    '''time complexity of n'''
     # loop over all array values until item is found
     for index, value in enumerate(array):
         if item == value:
@@ -17,16 +19,15 @@ def linear_search_iterative(array, item):
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
-    pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests
+    '''Time complexity of n'''
+    if array[index] == item:
+        return index
+    linear_search_recursive(array, item, index + 1)
 
 
 def binary_search(array, item):
-    """return the index of item in sorted array or None if item is not found"""
-    # implement binary_search_iterative and binary_search_recursive below, then
-    # change this to call your implementation to verify it passes all tests
+    """return the index of item in sorted array or None if item is not found.
+    Time complexity of log(n)"""
     return binary_search_iterative(array, item)
     # return binary_search_recursive(array, item)
 
